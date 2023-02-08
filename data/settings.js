dfe11e59fbd4035f524773e572f5f6bc37ac9b80
Jamie Penney <jamiepenney@users.noreply.github.com>
2023-02-09 10:54:32 +1300

    Set console logging level to debug

--- data/settings.js
+++ data/settings.js
@@ -1,3 +1,11 @@
 module.exports = {
-    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+    logging: {
+        // Console logging
+        console: {
+            level: "debug",
+            metrics: false,
+            audit: false
+        }
+    }
 }
